24a28cf3736adb0009ab3fb14f9740ee9a6e138c
Saranrajhub <sararajvijayakumar@gmail.com>
2025-03-12 18:34:12 +0530

    newjenkin

---
+++ src/test/java/com/jen/Testing1.java
@@ -0,0 +1,17 @@
+package com.jen;
+
+import org.testng.annotations.Test;
+
+public class Testing1 {
+	@Test
+	private void tc1() {
+		System.out.println("Jenkins1");
+		
+	}
+	@Test
+	private void tc2() {
+		System.out.println("Jenkins2");
+	}
+
+
+}
---
+++ src/test/java/com/jen/Testing2.java
@@ -0,0 +1,18 @@
+package com.jen;
+
+import org.testng.annotations.Test;
+
+public class Testing2 {
+	
+	@Test
+	private void tc3() {
+		System.out.println("Jenkins3");
+		
+	}
+	@Test
+	private void tc4() {
+		System.out.println("Jenkins4");
+	}
+
+
+}
